3a4dd2e75f2d586a4d5086d155a81d865e7a377b
tanusree00 <tanushreekaran99@gmail.com>
2024-05-27 22:33:32 +0530

    tanushree

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello tanu");
+import 'dotenv/config';
+
+import express, { urlencoded } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({extended:true}));
+app.get('/',(req,res)=>{
+    res.status(200).send('Node js is running...');
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server started at ${process.env.PORT}`);
+    
+});
